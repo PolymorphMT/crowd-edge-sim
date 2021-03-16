b119880ec657fce86e9927ba4f258a8f5b384c33
Mark Tyler <marktyler1997@gmail.com>
2021-03-16 16:24:33 +0000

    WIP add OLSR

--- crowdsrc-adhoc.cc
+++ crowdsrc-adhoc.cc
@@ -22,6 +22,7 @@
 #include "ns3/command-line.h"
 #include "ns3/config.h"
 #include "ns3/uinteger.h"
+#include "ns3/double.h"
 #include "ns3/string.h"
 #include "ns3/log.h"
 #include "ns3/yans-wifi-helper.h"
@@ -33,6 +34,10 @@
 #include "ns3/packet-socket-helper.h"
 #include "ns3/packet-socket-address.h"
 #include "ns3/netanim-module.h"
+#include "ns3/olsr-helper.h"
+#include "ns3/ipv4-static-routing-helper.h"
+#include "ns3/ipv4-list-routing-helper.h"
+#include "ns3/internet-stack-helper.h"
 
 using namespace ns3;
 
@@ -121,14 +126,43 @@ Experiment::SetupPacketReceive (Ptr<Node> node)
   return sink;
 }
 
+void 
+Experiment::SendPacket (Ptr<Socket> socket, uint32_t pktSize,
+                             uint32_t pktCount, Time pktInterval )
+{
+  if (pktCount > 0)
+    {
+      socket->Send (Create<Packet> (pktSize));
+      Simulator::Schedule (pktInterval, &SendPacket,
+                           socket, pktSize,pktCount - 1, pktInterval);
+    }
+  else
+    {
+      socket->Close ();
+    }
+}
+
+Ptr<Socket>
+Experiment::SetupPacketSend (Ptr<Node> node)
+{
+  TypeId tid = TypeId::LookupByName ("ns3::PacketSocketFactory");
+  Ptr<Socket> source = Socket::CreateSocket (node, tid);
+  source->Bind ();
+  source->SetRecvCallback (MakeCallback (&Experiment::SendPacket, this));
+  return source;
+}
+
 Gnuplot2dDataset
 Experiment::Run (const WifiHelper &wifi, const YansWifiPhyHelper &wifiPhy,
                  const WifiMacHelper &wifiMac, const YansWifiChannelHelper &wifiChannel)
 {
+  std::string phyMode ("DsssRate1Mbps");
+  Config::SetDefault ("ns3::WifiRemoteStationManager::NonUnicastMode",
+                      StringValue (phyMode));
   m_bytesTotal = 0;
 
   NodeContainer c;
-  c.Create (2);
+  c.Create (20);
 
   PacketSocketHelper packetSocket;
   packetSocket.Install (c);
@@ -147,6 +181,24 @@ Experiment::Run (const WifiHelper &wifi, const YansWifiPhyHelper &wifiPhy,
   mobility.SetMobilityModel ("ns3::ConstantPositionMobilityModel");
 
   mobility.Install (c);
+
+  // Enable OLSR
+
+  OlsrHelper olsr;
+  Ipv4StaticRoutingHelper staticRouting;
+
+  Ipv4ListRoutingHelper list;
+  list.Add (staticRouting, 0);
+  list.Add (olsr, 10);
+
+  InternetStackHelper internet;
+  internet.SetRoutingHelper (list); // has effect on the next Install ()
+  internet.Install (c);
+
+  Ipv4AddressHelper ipv4;
+  NS_LOG_INFO ("Assign IP Addresses.");
+  ipv4.SetBase ("10.1.1.0", "255.255.255.0");
+  Ipv4InterfaceContainer i = ipv4.Assign (devices);
 
   PacketSocketAddress socket;
   socket.SetSingleDevice (devices.Get (0)->GetIfIndex ());
@@ -181,12 +233,24 @@ int main (int argc, char *argv[])
   Experiment experiment;
   WifiHelper wifi;
   wifi.SetStandard (WIFI_PHY_STANDARD_80211a);
+  //wifi.EnableLogComponents ();  // Turn on all Wifi logging
   WifiMacHelper wifiMac;
   YansWifiPhyHelper wifiPhy = YansWifiPhyHelper::Default ();
+  wifiPhy.Set ("RxGain", DoubleValue (-10) );
+  wifiPhy.SetPcapDataLinkType (WifiPhyHelper::DLT_IEEE802_11_RADIO);
   YansWifiChannelHelper wifiChannel = YansWifiChannelHelper::Default ();
+  wifiChannel.SetPropagationDelay ("ns3::ConstantSpeedPropagationDelayModel");
+  wifiChannel.AddPropagationLoss ("ns3::FriisPropagationLossModel");
+  wifiPhy.SetChannel (wifiChannel.Create ());
   Gnuplot2dDataset dataset;
 
   wifiMac.SetType ("ns3::AdhocWifiMac");
+
+  // Add an upper mac and disable rate control
+  wifi.SetStandard (WIFI_PHY_STANDARD_80211b);
+  wifi.SetRemoteStationManager ("ns3::ConstantRateWifiManager",
+                                "DataMode",StringValue ("DsssRate1Mbps"),
+                                "ControlMode",StringValue ("DsssRate1Mbps"));
 
   NS_LOG_DEBUG ("54");
   experiment = Experiment ("54mb");
@@ -207,7 +271,7 @@ int main (int argc, char *argv[])
   wifi.SetRemoteStationManager ("ns3::ConstantRateWifiManager",
                                 "DataMode", StringValue ("OfdmRate36Mbps"));
   dataset = experiment.Run (wifi, wifiPhy, wifiMac, wifiChannel);
-  gnuplot.AddDataset (dataset);*/
+  gnuplot.AddDataset (dataset);
 
   NS_LOG_DEBUG ("24");
   experiment = Experiment ("24mb");
@@ -216,7 +280,7 @@ int main (int argc, char *argv[])
   dataset = experiment.Run (wifi, wifiPhy, wifiMac, wifiChannel);
   gnuplot.AddDataset (dataset);
 
-  /*NS_LOG_DEBUG ("18");
+  NS_LOG_DEBUG ("18");
   experiment = Experiment ("18mb");
   wifi.SetRemoteStationManager ("ns3::ConstantRateWifiManager",
                                 "DataMode", StringValue ("OfdmRate18Mbps"));
@@ -235,14 +299,14 @@ int main (int argc, char *argv[])
   wifi.SetRemoteStationManager ("ns3::ConstantRateWifiManager",
                                 "DataMode", StringValue ("OfdmRate9Mbps"));
   dataset = experiment.Run (wifi, wifiPhy, wifiMac, wifiChannel);
-  gnuplot.AddDataset (dataset);*/
+  gnuplot.AddDataset (dataset);
 
   NS_LOG_DEBUG ("6");
   experiment = Experiment ("6mb");
   wifi.SetRemoteStationManager ("ns3::ConstantRateWifiManager",
                                 "DataMode", StringValue ("OfdmRate6Mbps"));
   dataset = experiment.Run (wifi, wifiPhy, wifiMac, wifiChannel);
-  gnuplot.AddDataset (dataset);
+  gnuplot.AddDataset (dataset);*/
 
   gnuplot.GenerateOutput (std::cout);
 
@@ -277,7 +341,7 @@ int main (int argc, char *argv[])
   experiment = Experiment ("rraa");
   wifi.SetRemoteStationManager ("ns3::RraaWifiManager");
   dataset = experiment.Run (wifi, wifiPhy, wifiMac, wifiChannel);
-  gnuplot.AddDataset (dataset);*/
+  gnuplot.AddDataset (dataset);
 
   NS_LOG_DEBUG ("ideal");
   experiment = Experiment ("ideal");
@@ -285,7 +349,7 @@ int main (int argc, char *argv[])
   dataset = experiment.Run (wifi, wifiPhy, wifiMac, wifiChannel);
   gnuplot.AddDataset (dataset);
 
-  gnuplot.GenerateOutput (std::cout);
+  gnuplot.GenerateOutput (std::cout);*/
 
   return 0;
 }
